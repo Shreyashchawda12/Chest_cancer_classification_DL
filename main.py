cdc7d9714e5520e8031277a980cf61f4b66f2ec7
shreyashchawda12 <shreyashchawda12@gmail.com>
2024-06-16 16:02:51 +0530

    data_ingestion

---
+++ main.py
@@ -0,0 +1,14 @@
+from chest_cancer_classification_project import logger
+from chest_cancer_classification_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
